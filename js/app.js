52bc92195349ee1257cd857f98bcd810ef58f342
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-24 17:51:34 -0600

    crazyObject

--- js/app.js
+++ js/app.js
@@ -53,22 +53,94 @@ console.log("Works!");
 
 //2. TAKE IT EASY
 
-//1.  Make an array that holds all of the colors of the rainbow.
-const colorsOfTheRainbow = ["red", "orange", "yellow", "green", "blue", "purple", "red", "black"]
+// //1.  Make an array that holds all of the colors of the rainbow.
+// const colorsOfTheRainbow = ["red", "orange", "yellow", "green", "blue", "purple", "red", "black"]
 
-//2. Write code that will access "blue" from the rainbow array.
-console.log(colorsOfTheRainbow[4]);
+// //2. Write code that will access "blue" from the rainbow array.
+// console.log(colorsOfTheRainbow[4]);
 
-//3.  Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
-const susedCabrera = {
-	favoriteFood: "Chiles Rellenos", 
-	hobby: "running",
-	city: "Denver",
-	favoriteDatatype: "numbers"
+// //3.  Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+// const susedCabrera = {
+// 	favoriteFood: "Chiles Rellenos", 
+// 	hobby: "running",
+// 	city: "Denver",
+// 	favoriteDatatype: "numbers"
+// }
+
+// //4.  Write code that will access your hobby from the object that you just created.
+// console.log(susedCabrera.favoriteFood);
+
+
+
+//3. CRAZY OBJECT!
+
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favoriteHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
 }
 
-//4.  Write code that will access your hobby from the object that you just created.
-console.log(susedCabrera.favoriteFood);
+// Use crazyObject to log the following.
+
+// -"omg my mouth is burning"
+console.log(crazyObject.taco[1].salsa[5]);
+
+// -"Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
+
+// -"Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favoriteHobby);
+
+// -"Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
+
+// -The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
